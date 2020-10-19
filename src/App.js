ae3d5c7065d13fbaa3bb34f48cf3c5fe79476309
MJ Rossetti <s2t2@users.noreply.github.com>
2020-10-19 13:10:21 -0400

    Some buttons for example bootstrap integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+//import Navbar from 'react-bootstrap/Navbar'
+//import Nav from 'react-bootstrap/Nav'
+//import Container from 'react-bootstrap/Container'
+//import Row from 'react-bootstrap/Row'
+//import Col from 'react-bootstrap/Col'
+//import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
 
 import './App.css'
 //mport profilePhoto from './profile-photo.png'
+
+function clickHandler(event){
+    console.log("YOU CLICKED ME", event.target.value)
+}
 
 function App() {
     return (
@@ -10,7 +21,11 @@ function App() {
                 <h1>Hello World</h1>
             </header>
 
-             <p class="lead">There will be a page here.</p>
+             <p className="lead">There will be a page here.</p>
+
+            <Button onClick={clickHandler} value="research">Research</Button>
+            <Button onClick={clickHandler} value="teaching">Teaching</Button>
+            <Button onClick={clickHandler} value="experience">Experience</Button>
         </div>
     )
 }
